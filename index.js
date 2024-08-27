8ec2e39c74f5a40535c6e8f9e2d57a9c0b52933e
saSfa43g <157739294+saSfa43g@users.noreply.github.com>
2024-08-27 18:31:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,19 @@ const rolesallowed = config.roles
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 require('dotenv').config();
 
+const express = require("express")
+const app = express();
+var listener = app.listen(process.env.PORT || 2000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+app.listen(() => console.log("I'm Ready To Work..! 24H"));
+app.get('/', (req, res) => {
+  res.send(`
+  <body>
+  <center><h1>Bot 24H ON!</h1></center
+  </body>`)
+});
+
 client.on('ready', async () => {
   const { REST, Routes } = require('discord.js');
 
